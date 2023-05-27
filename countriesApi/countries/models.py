a169c5826e23b3d45d915f36861e2c4591e5d687
echeadle <echeadle@gmail.com>
2023-05-26 18:31:02 -0600

    Fixed views

--- countriesApi/countries/models.py
+++ countriesApi/countries/models.py
@@ -7,5 +7,9 @@ class Countries(models.Model):
     name = models.CharField(max_length=50, blank=False, default='')
     capital = models.CharField(max_length=50, blank=False, default='')
 
-    class Meta: ordering=('id',)
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering=('id',)
     
--- countriesApi/countries/views.py
+++ countriesApi/countries/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 from countries.models import Countries
 from countries.serializers import CountriesSerializer
-from rest_framework.decorators. import api_view
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
@@ -14,7 +14,7 @@ def countries_list(request):
     if request.method == 'GET':
         countries = Countries.objects.all()
 
-        name = request.Get.get('name', None)
+        name = request.GET.get('name', None)
         if name is not None:
             countries = countries.filter(name_icontains=name)
 
@@ -25,7 +25,38 @@ def countries_list(request):
     elif request.method == 'POST':
         countries_data = JSONParser().parse(request)
         countries_serializer = CountriesSerializer(data=countries_data)
-        if countries_serializer.is.valid():
+        if countries_serializer.is_valid():
             countries_serializer.save()
             return JsonResponse(countries_serializer.data, status.HTTP_201_CREATED)
-        JsonResponse(countries_serializer.errors, status.HTTP_400_BAD_REQUEST)
+        return JsonResponse(countries_serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def countries_detail(request, pk):
+    try:
+        country = Countries.objects.get(pk=pk)
+    except Countries.DoesNotExist:
+        return JsonResponse({'message': 'The country does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        countries_serializer = CountriesSerializer(country)
+        return JsonResponse(countries_serializer.data)
+    
+
+    elif request.method == 'PUT':
+        countries_data = JSONParder().parse(request)
+        countries_serializer = CountriesSerializer(countries, data=countries_data)
+        if countries_serializer.is_valid():
+            countries_serializer.save()
+            return JsonResponse(countries_serializer.data)
+        return JsonResponse(countries_serializer.errors, status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        countries.delete()
+        return JsonResponse({'message': 'Country was deleted successfully!'},
+                status=status.HTTP_204_NO_CONTENT)
+    
+
+
+
